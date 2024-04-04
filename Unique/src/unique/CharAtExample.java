270d57aa3c7c0ae7d4a4bf3496828dc8e9be44c9
Sunguva-user29 <sununava123@gmail.com>
2024-04-04 06:27:48 +0545

    CharAtExample

---
+++ Unique/src/unique/CharAtExample.java
@@ -0,0 +1,12 @@
+package unique;
+
+public class CharAtExample {
+    public static void main(String[] args){
+        String s="Object Oreinted Programming in Java";
+        char c1=s.charAt(0);
+        char c2=s.charAt(23);
+        System.out.println("character at index 0 is "+c1);
+        System.out.println("character at index 23 is "+c2);
+    }
+    
+}
