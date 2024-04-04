37ddc20cc1164e5bb20482253b0063189aa8fe6f
Sunguva-user29 <sununava123@gmail.com>
2024-04-04 06:45:02 +0545

    TOcharArray

---
+++ Unique/src/unique/ToCharArray.java
@@ -0,0 +1,10 @@
+package unique;
+
+public class ToCharArray {
+    public static void main(String[] args){
+        String s="OOP in JaVA";
+        char ch[]=s.toCharArray();
+        System.out.println(ch[2]+","+ch[4]);
+    }
+    
+}
