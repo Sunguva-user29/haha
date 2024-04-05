33ae08925f6d098eae97c6d34f0657de7026a9a3
Sunguva-user29 <sununava123@gmail.com>
2024-04-05 07:26:13 +0545

    INSERST AND ReVERSE

---
+++ Unique/src/stringbuffer/AppendExample.java
@@ -0,0 +1,9 @@
+public class AppendExample {
+    public static void main(String[] args)
+    {
+        StringBuffer s=new StringBuffer("BCA 3rd Semester");
+         System.out.println(s);
+         s.append(" OPP in java");
+         System.out.println(s);
+    }
+}
---
+++ Unique/src/stringbuffer/InsertExample.java
@@ -0,0 +1,11 @@
+public class InsertExample {
+    public static void main(String[] args)
+    {
+        StringBuffer s=new StringBuffer(" OOP in java BCA 3rd Semester");
+         System.out.println(s);
+         s.insert(12, " of");
+         System.out.println(s);
+         s.reverse();
+         System.out.println(s);
+    }
+}
