85ce1fdb21c9aaa132bdab05297ed757ff948c3e
Sunguva-user29 <sununava123@gmail.com>
2024-04-10 07:24:11 +0545

    MathExample

---
+++ Unique/src/JavaMathExample.java
@@ -0,0 +1,13 @@
+
+
+public class JavaMathExample {
+    public static void main(String[] args){
+        double x=25;
+        double y=5;
+        System.out.println("Maximum number of x and y is : "+Math.max(x, y));
+        System.out.println("Square root  of  y is : "+Math.sqrt(y));
+        System.out.println("power of x and y is: "+Math.pow(x, y));
+        System.out.println("Logarithm of x is : "+Math.log(x));
+        System.out.println("log10 of Y is : "+Math.log10(y));
+    }
+}
